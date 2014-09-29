8d260fb572972df466f45a0b687ed59c5a481aa5
Gabor Szabo <gabor@szabgab.com>
2014-09-29 11:53:47 +0300

    add command line script

--- lib/Net/Worker.pm
+++ lib/Net/Worker.pm
@@ -1,14 +1,27 @@
 package Net::Worker;
 use Moo;
+use MooX::Options;
+use 5.010;
 
 our $VERSION = '0.01';
+
+option verbose => (is => 'ro');
+#option file    => (is => 'ro', required => 1);
+ 
+sub run {
+    my ($self) = @_;
+    if ($self->verbose) {
+        say "Networking $VERSION";
+    }
+}
+ 
+
 
 =head NAME
 
 Net::Worker - checking network connectivity
 
 =cut
-
 
 1;
 
